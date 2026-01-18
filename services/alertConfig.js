cbf95640377592d025cfc2469ee4bb6e4eecb586
Chris Woolcott <christopher.woolcott@gmail.com>
2026-01-17 18:17:02 -0600

    new alerts

--- services/alertConfig.js
+++ services/alertConfig.js
@@ -4,25 +4,25 @@ module.exports = {
     {
       key: "tornado_warning",
       label: "Tornado Warning",
-      wav: "eas-alarm.wav",
+      wav: "alert_1_ready.wav",
       match: { event: "Tornado Warning" }
     },
     {
       key: "severe_thunderstorm_warning",
       label: "Severe Thunderstorm Warning",
-      wav: "eas-alarm.wav",
+      wav: "alert_2_ready.wav",
       match: { event: "Severe Thunderstorm Warning" }
     },
     {
       key: "flash_flood_warning",
       label: "Flash Flood Warning",
-      wav: "eas-alarm.wav",
+      wav: "alert_3_ready.wav",
       match: { event: "Flash Flood Warning" }
     },
     {
       key: "flood_warning",
       label: "Flood Warning",
-      wav: "eas-alarm.wav",
+      wav: "alert_4_ready.wav",
       match: { event: "Flood Warning" }
     }
   ]
