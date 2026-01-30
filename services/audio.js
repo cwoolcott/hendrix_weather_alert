3900212da84fdcd3f5bb24f9b04abcb46e5e0968
Chris Woolcott <christopher.woolcott@gmail.com>
2026-01-29 20:26:59 -0600

    audio fix

--- services/audio.js
+++ services/audio.js
@@ -5,8 +5,9 @@ function playWav(filename) {
   const soundFile = path.join(__dirname, "..", "sounds", filename);
   console.log("Playing WAV:", soundFile);
 
-  const p = spawn("aplay", ["-q", soundFile]);
-
+ // const p = spawn("aplay", ["-q", "-D", "plughw:2,0", soundFile]);
+ // const p = spawn("aplay", ["-q", "-D", "plughw:1,0", soundFile]);
+ const p = spawn("aplay", ["-q", "-D", "plughw:0,0", soundFile]);
   p.stdout.on("data", d => console.log("aplay out:", d.toString()));
   p.stderr.on("data", d => console.log("aplay err:", d.toString()));
 
